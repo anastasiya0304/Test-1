74937962c2e699fb2a17f1907bd9acec896d574a
skm2696 <skm03092014@gmail.com>
2017-12-18 02:32:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "Btree.h"
 #include "SplayTree.h"
-#include "CorrectST.h"
-#include "CorrectBT.h"
+#include "Correct.h"
+
 #include "Speed.h"
 #include <iostream>
 #include <string>
@@ -22,26 +22,17 @@ int main(int argc, char* argv[])
 	std::string inFile = argv[1];
 	std::string outFile = argv[2];
 
-	if (inFile == "001.dat.txt" || inFile == "003.dat.txt")
+	if (argv[3] != nullptr)
 	{
-		if (!CheckCorrectST(inFile, outFile))
-		{
-			std::cout << "Incorrect" << std::endl;;
-		}
-	}
-	
-	if (inFile == "002.dat.txt" || inFile == "004.dat.txt")
-	{
-		if (!CheckCorrectBT(inFile, outFile))
-		{
-			std::cout << "Incorrect" << std::endl;;
-		}
-	}
 
-	if (inFile == "005.dat.txt" || inFile == "006.dat.txt" || inFile == "007.dat.txt" || inFile == "008.dat.txt" || inFile == "009.dat.txt")
-	{
 		speed(inFile, outFile);
 	}
+	else
+	{
+		if (!CheckCorrect(inFile, outFile))
+			std::cout << "bjib";
+	}
 
+	system("pause");
 	return 0;
 }
